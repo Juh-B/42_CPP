99a3480521a4d58010fbc47dc08e039cec1d6b4d
Juliana Bonardi <106901927+Juh-B@users.noreply.github.com>
2025-12-04 17:50:45 -0300

    header ex02

--- CPP_00/ex02/Account.cpp
+++ CPP_00/ex02/Account.cpp
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcosta-b <jcosta-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/04 17:09:48 by jcosta-b          #+#    #+#             */
+/*   Updated: 2025/12/04 17:12:25 by jcosta-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <ctime>
 #include "Account.hpp"
 
-# define BLUE    "\033[34m"
-# define RESET   "\033[0m"
+# define BLUE  "\033[34m"
+# define RESET "\033[0m"
 
 // Initialize static attr.
 int Account::_nbAccounts = 0;
@@ -17,15 +29,15 @@ void	Account::_displayTimestamp( void )
 	std::time_t now = std::time(NULL);
 	std::tm *pt_time = std::localtime(&now);
 
-	std::cout << "["
-			  << (pt_time->tm_year + 1900)
-              << (pt_time->tm_mon < 9 ? "0" : "") << (pt_time->tm_mon + 1)
-              << (pt_time->tm_mday < 10 ? "0" : "") << pt_time->tm_mday
-              << "_"
-              << (pt_time->tm_hour < 10 ? "0" : "") << pt_time->tm_hour
-              << (pt_time->tm_min < 10 ? "0" : "") << pt_time->tm_min
-              << (pt_time->tm_sec < 10 ? "0" : "") << pt_time->tm_sec
-              << "] ";
+	std::cout	<< "["
+				<< (pt_time->tm_year + 1900)
+				<< (pt_time->tm_mon < 9 ? "0" : "") << (pt_time->tm_mon + 1)
+				<< (pt_time->tm_mday < 10 ? "0" : "") << pt_time->tm_mday
+				<< "_"
+				<< (pt_time->tm_hour < 10 ? "0" : "") << pt_time->tm_hour
+				<< (pt_time->tm_min < 10 ? "0" : "") << pt_time->tm_min
+				<< (pt_time->tm_sec < 10 ? "0" : "") << pt_time->tm_sec
+				<< "] ";
 }
 
 // Constructor -> With inicializer list
@@ -40,18 +52,18 @@ Account::Account(int initial_deposit)
 
 	_displayTimestamp();
 
-	std::cout << "index:" << BLUE << _accountIndex << RESET
-			  << ";amount:" << BLUE << _amount << RESET
-              << ";created" << std::endl;
+	std::cout	<< "index:" << BLUE << _accountIndex << RESET
+				<< ";amount:" << BLUE << _amount << RESET
+				<< ";created" << std::endl;
 }
 
 // Destructor
 Account::~Account(void)
 {
 	_displayTimestamp();
-	std::cout << "index:" << BLUE << _accountIndex << RESET
-			  << ";amount:" << BLUE << _amount << RESET
-			  << ";closed" << std::endl;
+	std::cout	<< "index:" << BLUE << _accountIndex << RESET
+				<< ";amount:" << BLUE << _amount << RESET
+				<< ";closed" << std::endl;
 }
 
 // Static Getters
@@ -68,11 +80,11 @@ int Account::getNbWithdrawals(void) { return _totalNbWithdrawals; }
 void Account::displayAccountsInfos(void)
 {
 	_displayTimestamp();
-	std::cout << "accounts:" << BLUE << getNbAccounts() << RESET
-			  << ";total:" << BLUE << getTotalAmount() << RESET
-			  << ";deposits:" << BLUE << getNbDeposits() << RESET
-			  << ";withdrawals:" << BLUE << getNbWithdrawals() << RESET
-			  << std::endl;
+	std::cout	<< "accounts:" << BLUE << getNbAccounts() << RESET
+				<< ";total:" << BLUE << getTotalAmount() << RESET
+				<< ";deposits:" << BLUE << getNbDeposits() << RESET
+				<< ";withdrawals:" << BLUE << getNbWithdrawals() << RESET
+				<< std::endl;
 }
 
 // Make deposit
@@ -86,12 +98,12 @@ void	Account::makeDeposit(int deposit)
 	_totalNbDeposits++;
 
 	_displayTimestamp();
-	std::cout << "index:" << BLUE << _accountIndex << RESET
-			  << ";p_amount:" << BLUE << previous << RESET
-			  << ";deposit:" << BLUE << deposit << RESET
-			  << ";amount:" << BLUE << _amount << RESET
-			  << ";nb_deposits:" << BLUE << _nbDeposits << RESET
-			  << std::endl;
+	std::cout	<< "index:" << BLUE << _accountIndex << RESET
+				<< ";p_amount:" << BLUE << previous << RESET
+				<< ";deposit:" << BLUE << deposit << RESET
+				<< ";amount:" << BLUE << _amount << RESET
+				<< ";nb_deposits:" << BLUE << _nbDeposits << RESET
+				<< std::endl;
 }
 
 // Make Withdrawal
@@ -100,8 +112,8 @@ bool	Account::makeWithdrawal(int withdrawal)
 	int	previous = _amount;
 
 	_displayTimestamp();
-	std::cout << "index:" << BLUE << _accountIndex << RESET
-			  << ";p_amount:" << BLUE << previous << RESET;
+	std::cout	<< "index:" << BLUE << _accountIndex << RESET
+				<< ";p_amount:" << BLUE << previous << RESET;
 
 	if (withdrawal > _amount)
 	{
@@ -113,10 +125,10 @@ bool	Account::makeWithdrawal(int withdrawal)
 	_totalAmount -=withdrawal;
 	_totalNbWithdrawals++;
 
-	std::cout << ";withdrawal:" << BLUE << withdrawal << RESET
-			  << ";amount:" << BLUE << _amount << RESET
-			  << ";nb_withdrawals:" << BLUE << _nbWithdrawals << RESET
-			  << std::endl;
+	std::cout	<< ";withdrawal:" << BLUE << withdrawal << RESET
+				<< ";amount:" << BLUE << _amount << RESET
+				<< ";nb_withdrawals:" << BLUE << _nbWithdrawals << RESET
+				<< std::endl;
 
 	return true;
 }
@@ -125,9 +137,9 @@ bool	Account::makeWithdrawal(int withdrawal)
 void	Account::displayStatus(void) const
 {
 	_displayTimestamp();
-	std::cout << "index:" << BLUE << _accountIndex << RESET
-			  << ";amount:" << BLUE << _amount << RESET
-			  << ";deposits:" << BLUE << _nbDeposits << RESET
-			  << ";withdrawals:" << BLUE << _nbWithdrawals << RESET
-			  << std::endl;
+	std::cout	<< "index:" << BLUE << _accountIndex << RESET
+				<< ";amount:" << BLUE << _amount << RESET
+				<< ";deposits:" << BLUE << _nbDeposits << RESET
+				<< ";withdrawals:" << BLUE << _nbWithdrawals << RESET
+				<< std::endl;
 }
